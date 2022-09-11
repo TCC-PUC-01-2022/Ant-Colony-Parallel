fefa64c940481dff1f931fc4786400b7255be1f2
Lucas Santiago <lu.santi.oli@gmail.com>
2022-09-10 21:03:15 -0300

    saving my parallel main

--- ant_colony.py
+++ ant_colony.py
@@ -5,6 +5,7 @@ from algorithms import ant_is_multithread
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="run the ant colony algorithm on the given csv")
+    parser.add_argument("column", help="which column needs to be searched")
     parser.add_argument("-j", "--jobs", type=int, help="number of threads to execute (default=1)")
     args = parser.parse_args() 
     
@@ -12,7 +13,7 @@ def main():
     
     # running the sequential version
     if not args.jobs:
-        ant_is.ant_colony(args.file)
+        ant_is.ant_colony(args.file, args.column)
 
     # running the parallel version
     else:
